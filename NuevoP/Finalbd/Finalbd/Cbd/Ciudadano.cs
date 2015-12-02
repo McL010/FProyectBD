dc104d3bcc7302c4f068a8d7c72072376ce6b8ec
Martin Castillo Lorenzo <Martin Castillo Lorenzo>
2015-12-01 19:38:13 -0800

    Proyecto corregido nuevo

---
+++ NuevoP/Finalbd/Finalbd/Cbd/Ciudadano.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Finalbd.Cbd
+{
+    
+   public class Ciudadano
+    {
+
+       //public class Ciudadano(){
+       
+       //this.Ciudadano = new ICollection<Ciudadano>();
+       
+       //}
+
+        [Key] public int IDCiudadano { get; set; }
+        public  string Nombre { get; set; }
+        public string sexo { get; set; }
+        public virtual ICollection<Sancion> Sanciones { get; set; }
+
+
+
+
+
+    }
+}
---
+++ NuevoP/Finalbd/Finalbd/Cbd/Sancion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Finalbd.Cbd
+{
+    public class Sancion
+    {
+        [Key]
+        public int IdS { get; set; }
+        public virtual int Precio { get; set; }
+        public virtual int IDAuto { get; set; }
+        public virtual int IDCiudadano { get; set; }
+        public virtual int IDAgente { get; set; }
+        public virtual int IDMulta { get; set; }
+        public DateTime Fec { get; set; }
+
+
+        public virtual ICollection<Ciudadano> Ciudadanos{ get; set; }
+    }
+}
